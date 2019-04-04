17b664293effa3a83fe4616240becea2a6b7ca3a
bratizgut <43445560+bratizgut@users.noreply.github.com>
2019-04-05 00:24:18 +0700

    31/31(fixed)

--- sem2/prim.c
+++ sem2/prim.c
@@ -40,14 +40,11 @@ void sort(int left, int right, GRAPH *s){
 GRAPH *findmin(GRAPH *minB, GRAPH *graph_in, int *count, int N, int M){
 	int min = INT_MAX;
 	minB = NULL;
-	for(int i = 0; i < M; i++){
+	for(int i = 0; i < M; i++)
 		if ((((count[graph_in[i].n1] == 1) && (count[graph_in[i].n2] == 0)) ||
 		((count[graph_in[i].n2] == 1) && (count[graph_in[i].n1] == 0))))
-			if((graph_in[i].weight <= min) && (graph_in[i].weight != -1)){
-			minB = &graph_in[i];
-			return minB;
-			}
-	}
+			if ((graph_in[i].weight <= min) && (graph_in[i].weight != -1))	
+				return &graph_in[i];
 	return minB;	
 }
 
@@ -73,7 +70,7 @@ int prim(GRAPH *graph_in, int N, int M, FILE *out){
 
 int main(){
 	
-	int N, M;
+	int N, M, indicate = 0;
     FILE *in, *out;
     GRAPH *graph_in;
 
@@ -103,32 +100,40 @@ int main(){
 	
 	if((N == 1) || (M == 0))
 		return 0;
-
-    graph_in = (GRAPH*)malloc(M * sizeof(GRAPH));
-
-    for(int i = 0; i < M; i++){
-        if (fscanf(in, "%d %d %ld", &graph_in[i].n1, &graph_in[i].n2, &graph_in[i].weight) != EOF){
-        } else {
-            printf("bad number of lines");
-            return 0;
-        }
-        if((graph_in[i].n1 <= 0) || (graph_in[i].n2 <= 0) || (graph_in[i].n1 > N) || (graph_in[i].n2 > N)){
-            printf("bad vertex");
-            return 0;
-        }
-		if((graph_in[i].weight < 0) || (graph_in[i].weight > INT_MAX)){
-			printf("bad length");
-			return 0;
-		}
-    }
-	if(M > 0)
-		sort(0, M-1, graph_in);
 	
-	if (prim(graph_in, N, M, out) != N){
-		fclose(out);
-		out = fopen("out.txt","w");
-		fprintf (out,"no spanning tree");
-	}
+	do{
+		graph_in = (GRAPH*)malloc(M * sizeof(GRAPH));
+
+		for(int i = 0; i < M; i++){
+			if (fscanf(in, "%d %d %ld", &graph_in[i].n1, &graph_in[i].n2, &graph_in[i].weight) != EOF){
+			} else {
+				printf("bad number of lines");
+				indicate = 1;
+				break;
+			}
+			if((graph_in[i].n1 <= 0) || (graph_in[i].n2 <= 0) || (graph_in[i].n1 > N) || (graph_in[i].n2 > N)){
+				printf("bad vertex");
+				indicate = 1;
+				break;
+			}
+			if((graph_in[i].weight < 0) || (graph_in[i].weight > INT_MAX)){
+				printf("bad length");
+				indicate = 1;
+				break;
+			}
+		}
+		if(indicate)
+			break;
+		if(M > 0)
+			sort(0, M-1, graph_in);
+		
+		if (prim(graph_in, N, M, out) != N){
+			fclose(out);
+			out = fopen("out.txt","w");
+			fprintf (out,"no spanning tree");
+		}
+		break;
+	}while(1);
 	
 	fclose(out);
 	free(graph_in);
